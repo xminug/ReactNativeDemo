515e8653e9d66e6d6407602ff45227d3a6d80b51
xminug <1124571232@qq.com>
2019-05-13 20:02:14 +0800

    增加RN js 和RN ios工程

---
+++ ReactNativeDemo-iOS/ReactNativeDemo/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  ReactNativeDemo
+//
+//  Created by minug on 2019/5/13.
+//  Copyright © 2019 minug. All rights reserved.
+//
+
+import UIKit
+import React
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+
+}
+
